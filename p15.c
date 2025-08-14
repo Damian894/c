ac9aca3ccb5950e70272cd3d93273d6ddb5cd528
Damian894 <nicolaedonica74@gmail.com>
2025-08-14 16:19:11 +0200

    Create p15.c

---
+++ p15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// + - * / %
+// +1 increment
+// -1 decrement
+
+int main()
+{
+    int v1 = 11, v2 = 5, result;
+
+    result = v1 % v2;
+    printf("v1 %% v2 = %d\r\n", result);
+
+    return 0;
+}
