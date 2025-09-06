31b1af3a76d8d690aa1bf3105c2b9fb4fb794054
Damian894 <nicolaedonica74@gmail.com>
2025-09-06 14:09:53 +0200

    p27.c modified

--- p27.c
+++ p27.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
+void change_me(int a[])
+{
+	a[0] = 89;
+	printf("change_me sa efectuat!\r\n");
+}
+
 int main(void)
 {
-	char note[5];
+	int a[4] = {0};
 
-	note[0] = 10;
-	note[1] = 8;
-	note[2] = 4;
-	note[3] = 8;
-	note[4] = 9;
+	printf("a[0] = %d\r\n", a[0]);
+	change_me(a);
+	printf("a[0] = %d\r\n", a[0]);
 
-	printf("nota1 = %d\r\n", note[0]);
-	printf("nota2 = %d\r\n", note[1]);
-	printf("nota3 = %d\r\n", note[2]);
-	printf("nota4 = %d\r\n", note[3]);
-	printf("nota5 = %d\r\n", note[4]);
 	return 0;
 }
