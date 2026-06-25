eac065378e2ad00d15e0898c80a989667aa8ae57
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 <nicolaedonica74@gmail.com>
2026-06-25 13:13:23 +0200

    coment p7.c

--- p7.c
+++ p7.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/*
+	Comentariile
+	Last Modification of:
+	25 June 2026 at 13 : 12
+*/
 int main()
 {
 	printf("Noi studiem\r\n");
+	printf("Comentariile :)");
+	// Eu afisez niste randuri
 	return 0;
 }
