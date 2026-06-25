e4350f123d78eeaae4a06774089b69a60d0813b4
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 <nicolaedonica74@gmail.com>
2026-06-25 14:24:33 +0200

    coment p8.c

--- p8.c
+++ p8.c
@@ -10,6 +10,13 @@ int main()
 {
 	int box1 = 0;
 	int box2 = 390;
+	/*
+	Variabilele ele nu pot incepe cu un numar sau cu un caracter special,
+	dar pot incepe cu underscore (_),
+	tot nu putem avea doua variabile ca box
+	e interzis de avea int box si int box in acelasi bloc
+	dar putem de exemplu box1 si box2
+	*/
 
 	printf("box1 = %d\t box2 = %d", box1, box2);
 	return 0;// result OK
