90e7602f99e6f26c7dbfa16b8d1aff193be00382
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 <nicolaedonica74@gmail.com>
2026-06-25 14:39:08 +0200

    recoment p8.c

--- p8.c
+++ p8.c
@@ -15,9 +15,13 @@ int main()
 	dar pot incepe cu underscore (_),
 	tot nu putem avea doua variabile ca box
 	e interzis de avea int box si int box in acelasi bloc
-	dar putem de exemplu box1 si box2
+	dar putem de exemplu box1 si box2,
+	mai este interzis sa facem spatiu la o variabila
+	remplasam aste pe underscore (_),
+	Si litere mari nu este de dorit la variabila
+	dar in afara in gilimele in pritf desigur
 	*/
 
-	printf("box1 = %d\t box2 = %d", box1, box2);
+	printf("Box1 = %d\t Box2 = %d", box1, box2);
 	return 0;// result OK
 }
